94dd0593bada797fa9e6c912bc82503c97ca4c57
nicholschm <77590673+nicholschm@users.noreply.github.com>
2022-04-28 14:46:09 -0400

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Day 1 - Began to brainstorm ideas for application I want to create.
+This is a test
+To see what happens
---
+++ psn_digital_deals_tracker.py
@@ -0,0 +1,4 @@
+# Author: Nicholas Schmidt
+# Date Last Modified: 04/28/2022
+# Description: This program will scrape deals from the PSN store from my selected favorite games, and keep track of
+#               their pricing changes over time. This should ping the PSN store 1 time per day at a selected time.
